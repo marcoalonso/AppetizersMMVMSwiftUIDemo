30acdd65236fda40135a44ed981b6cc0a9fb8f90
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-05 21:33:28 -0600

    Getting Started

---
+++ AppetizersMMVMSwiftUIDemo/AppetizersMMVMSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersMMVMSwiftUIDemoApp.swift
+//  AppetizersMMVMSwiftUIDemo
+//
+//  Created by Marco Alonso Rodriguez on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersMMVMSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizersMMVMSwiftUIDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppetizersMMVMSwiftUIDemo
+//
+//  Created by Marco Alonso Rodriguez on 05/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
